4b313f60fd0727b472235703607493ba9ac633ea
thotenn <joseenriquegaray@hotmail.com>
2021-10-26 00:50:28 -0300

    app students

--- kernel/set/base.py
+++ kernel/set/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.students',
     'apps.courses',
     'public.apps.PublicConfig'
 ]
